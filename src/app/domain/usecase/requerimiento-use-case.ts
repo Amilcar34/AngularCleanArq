4d405680524f320e16f5f1d893860ebfef613e9b
aalberino <AmilcarAlberino@medife.com.ar>
2022-07-26 14:47:18 -0300

    algunos cambios

--- src/app/domain/usecase/requerimiento-use-case.ts
+++ src/app/domain/usecase/requerimiento-use-case.ts
@@ -3,22 +3,19 @@ import { Observable } from "rxjs";
 import { Requerimiento } from "../models/requerimiento";
 import { RequerimientoGateway } from "../models/requerimiento/gateway/requerimiento-gateway";
 
-
 @Injectable({
     providedIn: 'root'
 })
 export class RequerimientoUseCase {
 
-    constructor(private requerimientoGateway: RequerimientoGateway) { }
+  constructor(private requerimientoGateway: RequerimientoGateway) { }
 
-    save(requerimiento: Requerimiento) : Observable<Requerimiento> {
-        throw new Error('Method not implemented.');
-    }
+//   save(requerimiento: Requerimiento) : Observable<Requerimiento> {
+//     throw new Error('Method not implemented.');
+//   }
 
-    getByID(id : number) : Observable<Requerimiento> {
-        return this.requerimientoGateway.getByID(id);
-    }
+//   getByID(id : number) : Observable<Requerimiento> {
+//     return this.requerimientoGateway.getByID(id);
+//   }
 
-
-    
 }
